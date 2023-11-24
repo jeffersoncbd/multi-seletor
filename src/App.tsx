1587056ba3b35443c231c975f09ab5e3dd52bfc6
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-11-24 13:44:45 -0300

    adiciona links

--- src/App.tsx
+++ src/App.tsx
@@ -8,60 +8,70 @@ const App: React.FC = () => {
   const [selected, setSelected] = React.useState<undefined | string>()
 
   return (
-    <div className='App'>
-      <ul className="left">
-        {left.sort().map((item) => <li key={item} className={item === selected ? 'selected' : undefined} onClick={() => setSelected(item)}>{item}</li>)}
-      </ul>
-      <div className="actions">
-        <button onClick={() => {
-          setSelected(undefined)
-          setRight([])
-          setLeft([...right, ...left])
-        }}>
+    <>
+      <div className="header">
+        <a href='https://github.com/jeffersoncbd/multi-seletor' target='_blank'>
+          Ver no GitHub
           <span className="material-symbols-rounded">
-            keyboard_double_arrow_left
+            open_in_new
           </span>
-        </button>
-        <button
-          className={selected === undefined ? 'blocked' : undefined}
-          onClick={() => {
-            const newRight = right.filter((item) => item !== selected)
-            setRight(newRight)
-            setLeft([...left, selected as string])
-            setSelected(undefined)
-          }}
-        >
-          <span className="material-symbols-rounded">
-            keyboard_arrow_left
-          </span>
-        </button>
-        <button
-          className={selected === undefined ? 'blocked' : undefined}
-          onClick={() => {
-            const newLeft = left.filter((item) => item !== selected)
-            setLeft(newLeft)
-            setRight([...right, selected as string])
-            setSelected(undefined)
-          }}
-        >
-          <span className="material-symbols-rounded">
-            keyboard_arrow_right
-          </span>
-        </button>
-        <button onClick={() => {
-          setSelected(undefined)
-          setLeft([])
-          setRight([...right, ...left])
-        }}>
-          <span className="material-symbols-rounded">
-            keyboard_double_arrow_right
-          </span>
-        </button>
+        </a>
       </div>
-      <ul className="right">
-        {right.sort().map((item) => <li key={item} className={item === selected ? 'selected' : undefined} onClick={() => setSelected(item)}>{item}</li>)}
-      </ul>
-    </div>
+      <div className='App'>
+        <ul className="left">
+          {left.sort().map((item) => <li key={item} className={item === selected ? 'selected' : undefined} onClick={() => setSelected(item)}>{item}</li>)}
+        </ul>
+        <div className="actions">
+          <button onClick={() => {
+            setSelected(undefined)
+            setRight([])
+            setLeft([...right, ...left])
+          }}>
+            <span className="material-symbols-rounded">
+              keyboard_double_arrow_left
+            </span>
+          </button>
+          <button
+            className={selected === undefined ? 'blocked' : undefined}
+            onClick={() => {
+              const newRight = right.filter((item) => item !== selected)
+              setRight(newRight)
+              setLeft([...left, selected as string])
+              setSelected(undefined)
+            }}
+          >
+            <span className="material-symbols-rounded">
+              keyboard_arrow_left
+            </span>
+          </button>
+          <button
+            className={selected === undefined ? 'blocked' : undefined}
+            onClick={() => {
+              const newLeft = left.filter((item) => item !== selected)
+              setLeft(newLeft)
+              setRight([...right, selected as string])
+              setSelected(undefined)
+            }}
+          >
+            <span className="material-symbols-rounded">
+              keyboard_arrow_right
+            </span>
+          </button>
+          <button onClick={() => {
+            setSelected(undefined)
+            setLeft([])
+            setRight([...right, ...left])
+          }}>
+            <span className="material-symbols-rounded">
+              keyboard_double_arrow_right
+            </span>
+          </button>
+        </div>
+        <ul className="right">
+          {right.sort().map((item) => <li key={item} className={item === selected ? 'selected' : undefined} onClick={() => setSelected(item)}>{item}</li>)}
+        </ul>
+      </div>
+    </>
   );
 }
 
